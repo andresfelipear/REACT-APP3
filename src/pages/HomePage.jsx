13534ae2c59698e4daff5b359ab6c826437265c9
Andres Arevalo <andresfelipear@gmail.com>
2022-04-21 05:31:29 -0700

    filter holidays per province, done!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,32 +10,59 @@ function HomePage() {
     const [countryFull, setCountryFull] = useState("")
     const [loading, setLoading] = useState(false)
     const [country, setCountry] = useState("CA")
+    const [province, setProvince] = useState("CA-BC")
+    const [provinces, setProvinces] = useState(
+        [{ code: 'CA-AB', name: 'Alberta' },
+        { code: 'CA-BC', name: 'British Columbia' },
+        { code: 'CA-MB', name: 'Manitoba' },
+        { code: 'CA-NB', name: 'New Brunswick' },
+        { code: 'CA-NL', name: 'Newfoundland and Labrador' },
+        { code: 'CA-NS', name: 'Nova Scotia' },
+        { code: 'CA-NT', name: 'Northwest Territories' },
+        { code: 'CA-NU', name: 'Nunavut' },
+        { code: 'CA-ON', name: 'Ontario' },
+        { code: 'CA-PE', name: 'Prince Edward Island' },
+        { code: 'CA-QC', name: 'Quebec' },
+        { code: 'CA-SK', name: 'Saskatchewan' },
+        { code: 'CA-YT', name: 'Yukon' }
+        ])
+
     const monthsYear = [
-        new Date('2021-01-01'.replace(/-/g,'/')),
-        new Date('2021-02-01'.replace(/-/g,'/')),
-        new Date('2021-03-01'.replace(/-/g,'/')),
-        new Date('2021-04-01'.replace(/-/g,'/')),
-        new Date('2021-05-01'.replace(/-/g,'/')),
-        new Date('2021-06-01'.replace(/-/g,'/')),
-        new Date('2021-07-01'.replace(/-/g,'/')),
-        new Date('2021-08-01'.replace(/-/g,'/')),
-        new Date('2021-09-01'.replace(/-/g,'/')),
-        new Date('2021-10-01'.replace(/-/g,'/')),
-        new Date('2021-11-01'.replace(/-/g,'/')),
-        new Date('2021-12-01'.replace(/-/g,'/')),
+        new Date('2021-01-01'.replace(/-/g, '/')),
+        new Date('2021-02-01'.replace(/-/g, '/')),
+        new Date('2021-03-01'.replace(/-/g, '/')),
+        new Date('2021-04-01'.replace(/-/g, '/')),
+        new Date('2021-05-01'.replace(/-/g, '/')),
+        new Date('2021-06-01'.replace(/-/g, '/')),
+        new Date('2021-07-01'.replace(/-/g, '/')),
+        new Date('2021-08-01'.replace(/-/g, '/')),
+        new Date('2021-09-01'.replace(/-/g, '/')),
+        new Date('2021-10-01'.replace(/-/g, '/')),
+        new Date('2021-11-01'.replace(/-/g, '/')),
+        new Date('2021-12-01'.replace(/-/g, '/')),
     ]
 
     const UrlApi = "https://holidayapi.com/v1"
     const YEAR = '2021'
 
     const fetchHolidays = useCallback(() => {
+        console.log(province)
         setLoading(true);
-        fetch(`${UrlApi}/holidays?pretty&key=${process.env.REACT_APP_API_KEY}&country=${country}&year=${YEAR}`, {
+        fetch(`${UrlApi}/holidays?pretty&key=${process.env.REACT_APP_API_KEY}&country=${country}&year=${YEAR}&subdivisions=true`, {
             method: "GET",
         }).then(async (response) => {
             if (response.ok) {
                 const data = await response.json();
-                setHolidays(data.holidays.filter(holiday => holiday.public));
+                const holidaysLegal = data.holidays.filter(holiday => holiday.public)
+                const canadaHolidays = holidaysLegal.filter(holiday => (holiday.subdivisions.length === 0))
+                const provinceHolidays = holidaysLegal.filter(holiday => {
+                    const holidayReal = holiday.subdivisions.filter(subdivision => subdivision === province)
+                    if (holidayReal.length >= 1) {
+                        return true;
+                    }
+                })
+                const totalHolidays = canadaHolidays.concat(provinceHolidays)
+                setHolidays(totalHolidays);
             }
             else {
                 // openModal("Error Post", "Error fetching data")
@@ -43,7 +70,7 @@ function HomePage() {
             }
             setLoading(false);
         }).catch(err => { setLoading(false) });
-    }, [country])
+    }, [country, province])
 
     useEffect(() => {
         if (holidays.length === 0) {
@@ -53,7 +80,8 @@ function HomePage() {
 
     useEffect(() => {
         fetchHolidays();
-    }, [country]);
+    }, [country, province]);
+
 
     const fetchCountries = useCallback(() => {
         setLoading(true);
@@ -78,12 +106,15 @@ function HomePage() {
         }
     }, [countries, fetchCountries]);
 
+
+
     useEffect(() => {
         if (countries.length !== 0) {
             const countryCode = countries.find(ctry => (ctry.name).toLowerCase() === countryFull.toLowerCase())
             if (countryCode) {
-                console.log(countryCode.code)
                 setCountry(countryCode.code)
+                setProvinces(countryCode.subdivisions)
+
             }
         }
     }, [countryFull]);
@@ -108,10 +139,26 @@ function HomePage() {
                     <Form.Input value={countryFull} type="text" name="country" placeholder='Search for your country' onChange={(e) => { setCountryFull(e.target.value) }} />
                 </Form.Control>
             </Form.Field>
+            <Form.Field>
+                <Form.Control>
+                    <Form.Select value={province} onChange={async (e) => {
+                        setProvince(e.target.value)
+                    }}>
+                        {provinces && (
+                            provinces.map(province => {
+                                const provinceName = province.name;
+                                return (
+                                    <option value={province.code} key={province.code}>{provinceName}</option>
+                                )
+                            })
+                        )}
+                    </Form.Select>
+                </Form.Control>
+            </Form.Field>
             <Columns gap={2} mt={6}>
                 {holidays.length !== 0 && (
                     monthsYear.map(month => {
-                        const holidaysMonth = holidays.filter(monthHolidays => new Date((monthHolidays.date).replace(/-/g,'/')).getMonth() === month.getMonth())
+                        const holidaysMonth = holidays.filter(monthHolidays => new Date((monthHolidays.date).replace(/-/g, '/')).getMonth() === month.getMonth())
                         return (
                             <Columns.Column display='flex' size={'one-third'} justifyContent='center' key={month.toString()}>
                                 <MonthlyCalendar date={month} arrayHolidays={holidaysMonth} />
