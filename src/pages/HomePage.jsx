1e2e88943795b2722a6bac4672e99876e459efdd
Andres Arevalo <andresfelipear@gmail.com>
2022-04-20 22:22:01 -0700

    fetch holidays per contry, done!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,34 +12,28 @@ function HomePage() {
     const COUNTRY = 'CA'
     const YEAR = '2021'
 
-    console.log(process.env.HOLIDAYS_API_KEY)
+    const fetchHolidays = useCallback(() => {
+        setLoading(true);
+        fetch(`${UrlApi}?pretty&key=${process.env.REACT_APP_API_KEY}&country=${COUNTRY}&year=${YEAR}`, {
+            method: "GET",
+        }).then(async (response) => {
+            if (response.ok) {
+                const data = await response.json();
+                setHolidays(data.holidays.filter(holiday=>holiday.public));
+            }
+            else {
+                // openModal("Error Post", "Error fetching data")
+                console.log('hola')
+            }
+            setLoading(false);
+        }).catch(err => { setLoading(false) });
+    }, [holidays])
 
-    // const fetchHolidays = useCallback(() => {
-    //     setLoading(true);
-    //     fetch(`${UrlApi}?pretty&key=${process.env.HOLIDAYS_API_KEY}&country=${COUNTRY}&year=${YEAR}`, {
-    //         method: "GET",
-    //         credentials: "include",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //     }).then(async (response) => {
-    //         if (response.ok) {
-    //             const data = await response.json();
-    //             setHolidays(data.holidays);
-    //         }
-    //         else {
-    //             // openModal("Error Post", "Error fetching data")
-    //             console.log('hola')
-    //         }
-    //         setLoading(false);
-    //     }).catch(err => { setLoading(false) });
-    // }, [holidays])
-
-    // useEffect(() => {
-    //     if (holidays.length === 0) {
-    //         fetchHolidays();
-    //     }
-    // }, [holidays, fetchHolidays]);
+    useEffect(() => {
+        if (holidays.length === 0) {
+            fetchHolidays();
+        }
+    }, [holidays, fetchHolidays]);
 
     if (loading) {
         return (
